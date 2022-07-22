d6e2c3c59369b638f06a39275c69d71483082e28
Trevor Esparza <trevoresparza30@gmail.com>
2022-07-22 17:34:52 -0500

    through aysnc/await

--- src/App.js
+++ src/App.js
@@ -1,34 +1,77 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    /* FETCHING FROM API CAN BE  JUST LIKE JAVASCRIPT--- OR WE CAN USE AXIOS---NEED TO LOOK UP*/
+
+    // function fetchMoviesHandler(){
+    //    fetch('https://swapi.dev/api/films/')
+    //       .then(res => {
+    //           return res.json()
+    //       }).then(data => {
+    //           const transformedMovies = data.results.map(movieData => {
+    //               return {
+    //                   id : movieData.episode_id,
+    //                   title : movieData.title,
+    //                   openingText : movieData.opening_crawl,
+    //                   releaseDate : movieData.release_date
+    //               };
+    //           });
+    //            setMovies(transformedMovies);
+    //   });
+    // }
+
+    async function fetchMoviesHandler() {
+        setIsLoading(true);
+        setError(null);
+
+        try {
+            const res = await fetch('https://swapi.dev/api/films/')
+
+            if(!res.ok){
+                throw new Error('Oops...Something went wrong!');
+            }
+            const data = await res.json();
+            const transformedMovies = data.results.map(movieData => {
+                return {
+                    id: movieData.episode_id,
+                    title: movieData.title,
+                    openingText: movieData.opening_crawl,
+                    releaseDate: movieData.release_date
+                };
+            });
+            setMovies(transformedMovies);
+        } catch (error){
+            setError(error.message);
+        }
+        setIsLoading(false);
+    }
+
+    let content = <p>Trying to connect.....</p>;
+    if(movies.length > 0){
+        content = <MoviesList movies={movies}/>;
+    }
+    if(error){
+        content = <p>{error}</p>
+    }
+    if(isLoading){
+        content = <p>LOADING........ </p>;
+    }
+
+    return (
+        <React.Fragment>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>{content}</section>
+        </React.Fragment>
+    );
 }
 
 export default App;
